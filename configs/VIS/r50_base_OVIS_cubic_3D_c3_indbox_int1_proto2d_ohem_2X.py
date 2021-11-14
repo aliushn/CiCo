b6d175a9c55888f6298012f0145f510237d919d3
lmh <MinghanLi.liminghan0330@gmail.com>
2021-11-14 11:28:42 +0800

    prepare anonymous1

---
+++ configs/VIS/r50_base_OVIS_cubic_3D_c3_indbox_int1_proto2d_ohem_2X.py
@@ -0,0 +1,44 @@
+_base_ = ['configs/_base_/models/r50_base.yaml', 'configs/VIS/base_VIS.py']
+
+MODEL = dict(
+    BACKBONE=dict(
+        CONV_BODY='ResNet50',
+        PATH='STMask_resnet50_coco_960_46_340000.pth'),
+
+    PREDICTION_HEADS=dict(
+        CUBIC_MODE=True,
+        CUBIC_3D_MODE=True,
+        CUBIC_MODE_ON_PROTONET=False,
+        CUBIC_CORRELATION_MODE=False,
+        # INITIALIZATION=reduced or inflated
+        CUBIC_MODE_WITH_INITIALIZATION='inflated'),
+
+    CLASS_HEADS=dict(
+        USE_FOCAL_LOSS=False)
+)
+
+DATASETS = dict(
+    TYPE='vis',
+    NUM_CLASSES=25,
+    TRAIN='train_OVIS_dataset',
+    VALID_SUB='valid_sub_OVIS_dataset',
+    VALID='valid_OVIS_dataset',
+    TEST='test_OVIS_dataset')
+
+CiCo = dict(
+    FRAME2CLIP_EXPAND_PROPOSALS=True)
+
+SOLVER = dict(
+    IMS_PER_BATCH=24,
+    NUM_CLIP_FRAMES=3,
+    NUM_CLIPS=1,
+    LR_STEPS=(8, 14),
+    MAX_EPOCH=20)
+
+TEST = dict(
+    IMS_PER_BATCH=1,
+    NUM_CLIP_FRAMES=3,
+    NMS_CONF_THRESH=0.2)
+
+OUTPUT_DIR = 'weights/OVIS/'
+NAME = 'r50_base_OVIS_cubic_3D_c3_indbox_int1_proto2d_ohem_2X'
